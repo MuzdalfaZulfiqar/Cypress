4c6b562a933774cd8be9e9347cc31a68685aea40
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-12-08 20:28:03 +0500

    Pushed initial setup

---
+++ cypress/e2e/devsta.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+describe("DevSta Smoke Test", () => {
+
+  beforeEach(() => {
+    cy.visit(Cypress.env("baseUrl"));
+  });
+
+  it("Loads the DevSta homepage", () => {
+    cy.contains("DevSta").should("be.visible");
+  });
+
+  it("Opens login page", () => {
+    cy.contains("Login").click();
+    cy.url().should("include", "/login");
+  });
+
+});
